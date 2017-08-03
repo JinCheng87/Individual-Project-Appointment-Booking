fc2860e782d5bcc0da164bc819b98e5b1e4cfd7f
Jin Cheng <jincheng88@outlook.com>
2017-08-02 19:09:35 -0500

    set up rspec, get the first test done

---
+++ spec/controller/home_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+require 'database_cleaner'
+DatabaseCleaner.strategy = :truncation
+RSpec.describe HomeController, type: :controller do
+  render_views
+
+  let(:store_params) { {name: Faker::Company.name, location: "Faker::Address.street_address", hours: '9AM-9PM', description: "An Exclusive SPA offering luxurious cacials & therapeutic massages."} }
+
+  describe 'GET #index' do
+    it "list all the stores" do
+      store1 = Store.create!(store_params)
+      store2 = Store.create!(store_params)
+      store3 = Store.create!(store_params)
+
+      get :index
+
+      expect(Store.count).to eq(3)
+      expect(response.body).to include("img-circle")
+      expect(response.body).to include(store2.location)
+    end
+  end
+
+  DatabaseCleaner.clean
+end
