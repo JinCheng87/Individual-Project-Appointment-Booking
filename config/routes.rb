9b4524bb834ddb954d776f10b925e9b7ab62167d
Jin Cheng <jincheng88@outlook.com>
2017-08-07 18:29:07 -0500

    add redis to background

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
-
   root to:'home#index'
 
   get '/admin', to: 'admin#index'
