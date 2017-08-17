7b4991c5941fe485d18007b3acffb209953aac44
Jin Cheng <jincheng88@outlook.com>
2017-08-16 22:05:31 -0500

    add rake task to delete past blocktime data every hour

--- lib/tasks/clean_block_times.rb
+++ lib/tasks/clean_block_times.rb
@@ -1,3 +1,14 @@
 namespace :clean_block_times do
-  desc "delete all the block times data one day before"
+  desc "delete all the block times data one day before today"
+  task clean: :environment do
+    block_time_array = []
+    Staff.all.each do |staff|
+      block_time_array<<staff.block_times.where("start_time <= :one_hour_before",{one_hour_before: Time.zone.now - 1.hour})
+    end
+
+    block_time_array.flatten.each do |block_time|
+      @block_time = block_time
+      @block_time.destroy
+    end
+  end
 end
