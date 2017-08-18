0677f58defa39ec413ca5523bbe2c2af16613d59
Jin Cheng <jincheng88@outlook.com>
2017-08-17 20:24:27 -0500

    when block or unblock it will go back to current page

--- app/controllers/block_times_controller.rb
+++ app/controllers/block_times_controller.rb
@@ -6,14 +6,17 @@ class BlockTimesController < ApplicationController
     if !@staff.is_blocked(start_time).any?
       @staff.block_times.create!(start_time: start_time, end_time: end_time)
     end
-    redirect_to store_schedule_path(@staff.store_id, Date.today)
+    redirect_to store_schedule_path(@staff.store_id, start_time.strftime('%Y-%m-%d'))
   end
 
   def destroy
     find_staff
     @block_time = BlockTime.find(params[:block_time_id])
+    go_time = @block_time[0].start_time
     @block_time[0].destroy
-    redirect_to store_schedule_path(@staff.store_id, Date.today)
+    redirect_to store_schedule_path(@staff.store_id, go_time.strftime('%Y-%m-%d'))
+  rescue ActiveRecord::RecordNotFound
+    render '/error/not_found'
   end
 
   private
