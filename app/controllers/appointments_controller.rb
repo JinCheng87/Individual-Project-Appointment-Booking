faa79fa1401f39a8470838ba291e5f3571e2bfee
Jin Cheng <jincheng88@outlook.com>
2017-09-03 23:27:20 -0500

    created service object

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -38,12 +38,8 @@ class AppointmentsController < ApplicationController
         redirect_to store_appointment_path(@store,@appointment,token: @appointment.token), notice: 'Appointment created successfully'
         UserMailer.confirm_appointment(@appointment).deliver_now
 
-        client = Twilio::REST::Client.new
-        client.messages.create({
-        from: Rails.application.secrets.twilio_phone,
-        to: '+16469155917', # your sign up phone number
-        body: 'test?',
-        })
+        service = SendTextMessage.new('+16469155917','text')
+        service.send_message
 
 
 
